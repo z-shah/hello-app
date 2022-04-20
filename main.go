a0e35506b1bd04d4a875b5d418b283bdb4a84959
Zaynab Shah <zaynabs@google.com>
2022-04-20 15:31:27 +1000

    update version

--- main.go
+++ main.go
@@ -47,7 +47,7 @@ func hello(w http.ResponseWriter, r *http.Request) {
 	host, _ := os.Hostname()
 	fmt.Fprintf(w, "Hello, world!\n")
 	fmt.Fprintf(w, "This is the new version!!!\n")
-	fmt.Fprintf(w, "Version: 2.0.0\n")
+	fmt.Fprintf(w, "Version: 3.0.0\n")
 	fmt.Fprintf(w, "Hostname: %s\n", host)
 }
 
